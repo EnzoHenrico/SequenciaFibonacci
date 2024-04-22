3a536f7bce87c9249d893cdaf1de6ffc70b92896
EnzoHenrico <enzo.mascia1@gmail.com>
2024-04-22 16:12:33 -0300

    Add project files.

---
+++ SequenciaFibonacci/Program.cs
@@ -0,0 +1,13 @@
+﻿int final = 0, num1 = 1, num2 = 0, acc = 0;
+
+Console.Write("Diga o indice de parada da sequencia de Fibonacci: ");
+final = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Sequencia Fibonacci: ");
+for (int i = 1; i <= final; i++)
+{   
+    acc = num1 + num2;
+    num1 = num2;
+    num2 = acc;
+    Console.WriteLine(" " + num2);
+}
